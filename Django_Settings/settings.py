826291fc32fb1aa58cff0f7abb0a3ddd1c062ef4
Eviera16 <64331763+Eviera16@users.noreply.github.com>
2022-01-10 23:32:26 -0500

    add cors back

--- Django_Settings/settings.py
+++ Django_Settings/settings.py
@@ -42,9 +42,12 @@ INSTALLED_APPS = [
     'Django_Chat',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,6 +61,14 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+        'https://tuck-chat-server.herokuapp.com',
+    ]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://tuck-chat-server.herokuapp.com',
+    ]
 
 
 ROOT_URLCONF = 'Django_Settings.urls'
